e88171de2d6268647d814ee4876bc8393dc9c613
SAIRAM299 <psairam299gmail.com>
2023-12-30 19:11:59 +0530

    g

--- src/Swip/swips.jsx
+++ src/Swip/swips.jsx
@@ -1,10 +1,63 @@
 import React from 'react';
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import { EffectCoverflow, Pagination } from 'swiper/modules';
+import slide_image_1 from '../assets/mi.jpg';
+import slide_image_2 from '../assets/mi.jpg';
+import slide_image_3 from '../assets/mi.jpg';
+import slide_image_4 from '../assets/mi.jpg';
+import slide_image_5 from '../assets/mi.jpg';
+import slide_image_6 from '../assets/mi.jpg';
+import slide_image_7 from '../assets/mi.jpg'
+import './swips.css'
 const Swips = () => {
     return (
-        <div>
-            jhjmgf
-        </div>
+       
+
+    <div className='containg'>
+        <h1 className='tid'>Warlord of Sea </h1>
+      <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        loop={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 2.5,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src={slide_image_1} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_2} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_3} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_4} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_5} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_6} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_7} />
+        </SwiperSlide>
+      </Swiper>
+    </div>
     );
 }
 
